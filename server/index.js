81679a6bbe79ec72fd80b73ef19d065cfe6836d1
raview69 <raviwicaksono@aol.com>
2022-05-23 04:45:39 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -19,10 +19,7 @@ database.on('error', (error) => {
 database.once('connected', () => {
     console.log('Database Connected')
 })
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    next()
-})
+
 app.use(express.json())
 app.use('/api', routes)
 
