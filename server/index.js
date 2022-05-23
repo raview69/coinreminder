c0f2b846bba24dd082d91d7d30e901be90c9b53f
raview69 <raviwicaksono@aol.com>
2022-05-23 21:42:33 +0700

    update .env, .gitignore, server/index.js and package.json update Search.js and server/index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const axios = require('axios').default
+var cors = require('cors')
 require('dotenv').config()
 const mongoString = process.env.DATABASE_URL
 mongoose.connect(mongoString)
@@ -20,6 +21,7 @@ database.once('connected', () => {
     console.log('Database Connected')
 })
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', routes)
 
